962e984ec75fdfd1e06bfa8ca4e3f736052a689e
lanly_dev <lanly-dev@users.noreply.github.com>
2017-11-26 03:50:43 -0800

    reset project

--- out/test/index.js
+++ out/test/index.js
@@ -1,3 +1,4 @@
+"use strict";
 //
 // PLEASE DO NOT MODIFY / DELETE UNLESS YOU KNOW WHAT YOU ARE DOING
 //
@@ -9,7 +10,8 @@
 // host can call to run the tests. The test runner is expected to use console.log
 // to report the results back to the caller. When the tests are finished, return
 // a possible error to the callback or null if none.
-var testRunner = require('vscode/lib/testrunner');
+Object.defineProperty(exports, "__esModule", { value: true });
+const testRunner = require("vscode/lib/testrunner");
 // You can directly control Mocha options by uncommenting the following lines
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
 testRunner.configure({
