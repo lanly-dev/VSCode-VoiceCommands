217c396a12f23e0ef1124ee225800e0c980397dc
lanly_dev <lanly-dev@users.noreply.github.com>
2018-01-01 14:52:05 -0800

    MS Speech old code

--- backend/MS Speech/Program.cs
+++ backend/MS Speech/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Speech.Recognition;
 
-namespace MS_Speech
+namespace WordMatching
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        // The main entry point for the application.
+        [STAThread]
+        static void Main(String[] args)
         {
-            Console.WriteLine("Hello World!");
+            SpeechRecognitionEngine sre = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
+            // String path = args[0] + "\\GrammarList.txt";
+            String path = "GrammarList.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
+            Choices words = new Choices();
+            // Console.WriteLine(lines.Length);
+            words.Add(lines);
+
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(words);
+
+            // Create the Grammar instance.
+            Grammar g = new Grammar(gb);
+            sre.LoadGrammar(g);
+            sre.SetInputToDefaultAudioDevice();
+            sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized);
+
+            while (true)
+            {
+                sre.Recognize();
+            }
+        }
+        static void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            Console.WriteLine(e.Result.Text);
         }
     }
 }
