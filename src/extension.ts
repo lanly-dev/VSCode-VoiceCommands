cb09d8d5b8c6ded26fdb86186372b55d22f8aab8
lanly_dev <lanly-dev@users.noreply.github.com>
2019-01-26 16:24:01 -0800

    Change format style

--- src/extension.ts
+++ src/extension.ts
@@ -1,174 +1,175 @@
-'use strict';
+'use strict'
 
-import * as vscode from 'vscode';
-import * as child_process from 'child_process';
-import { window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument, TextEditor } from 'vscode';
+import * as vscode from 'vscode'
+import * as child_process from 'child_process'
 
 export function activate(context: vscode.ExtensionContext) {
-    let checkJRE = false;
-    let init_disposable = commands.registerCommand('start', () => {
-        // window.showInformationMessage('This is Voice Command! activated');
-        var spawn = child_process.spawn('java', ['-version']).on('error',
-            err => { window.showInformationMessage('Please install JRE in order to run this extension!!!') })
-        spawn.stderr.on('data', (data: Buffer) => {
-            if (data.indexOf('version') >= 0)
-                checkJRE = true
-            spawn.kill()
-        });
-        spawn.on('exit', (code, signal) => {
-            if (checkJRE == true) {
-                if (process.platform === 'win32') {
-                    let vl = new VoiceListener(context, 'other');
-                    vl.run();
-                }
-                else {
-                    let vl = new VoiceListener(context, 'other');
-                    vl.run();
-                }
-            }
-            else
-                window.showInformationMessage('Please install JRE in order to run this extension!!!');
-        });
-    });
-    context.subscriptions.push(init_disposable);
+  let checkJRE = false
+  let init_disposable = vscode.commands.registerCommand('start', () => {
+    // window.showInformationMessage('This is Voice Command! activated');
+    var spawn = child_process.spawn('java', ['-version']).on('error', err => {
+      vscode.window.showInformationMessage(
+        'Please install JRE in order to run this extension!!!'
+      )
+    })
+    spawn.stderr.on('data', (data: Buffer) => {
+      if (data.indexOf('version') >= 0) checkJRE = true
+      spawn.kill()
+    })
+    spawn.on('exit', (code, signal) => {
+      if (checkJRE == true) {
+        if (process.platform === 'win32') {
+          let vl = new VoiceListener(context, 'other')
+          vl.run()
+        } else {
+          let vl = new VoiceListener(context, 'other')
+          vl.run()
+        }
+      } else
+        vscode.window.showInformationMessage(
+          'Please install JRE in order to run this extension!!!'
+        )
+    })
+  })
+  context.subscriptions.push(init_disposable)
 }
 
 class VoiceListener {
+  private sysType: String
+  private execFile
+  private child
+  private sttbar: SttBarItem
 
-    private sysType: String;
-    private execFile;
-    private child;
-    private sttbar: SttBarItem;
+  constructor(context: vscode.ExtensionContext, type: String) {
+    this.sysType = type
+    this.execFile = child_process.spawn
+    this.sttbar = new SttBarItem()
+    let disposable1 = vscode.commands.registerCommand('toggle', () => {
+      if (this.sttbar.getSttText() == 'on') {
+        this.sttbar.off()
+        this.killed()
+      } else {
+        this.sttbar.on()
+        this.run()
+      }
+    })
+    let disposable2 = vscode.commands.registerCommand('stop_listen', () => {
+      this.sttbar.off()
+      this.killed()
+    })
+    context.subscriptions.push(disposable1)
+    context.subscriptions.push(disposable2)
+    this.sttbar.setSttCmd('toggle')
+  }
+  run() {
+    if (this.sysType == 'win')
+      // console.log(this.child = this.execFile(__dirname + '/WordsMatching.exe'));
+      this.child = this.execFile(__dirname + '/WordsMatching.exe')
+    else
+      this.child = this.execFile('java', [
+        '-jar',
+        __dirname + '/WordsListener.jar'
+      ]).on('error', err => {
+        vscode.window.showInformationMessage('Something went wrong!!! Sorry 😢')
+      })
+    this.child.stdout.on('data', data => {
+      vscode.window.setStatusBarMessage(data.toString(), 1000)
+      let centralCmd = new commandsClass()
+      // console.log(data.toString());
+      centralCmd.runCmd(data.toString().trim())
+    })
 
-    constructor(context: vscode.ExtensionContext, type: String) {
-        this.sysType = type;
-        this.execFile = child_process.spawn;
-        this.sttbar = new SttBarItem();
-        let disposable1 = commands.registerCommand('toggle', () => {
-            if (this.sttbar.getSttText() == 'on') {
-                this.sttbar.off();
-                this.killed();
-            }
-            else {
-                this.sttbar.on();
-                this.run();
-            }
-        });
-        let disposable2 = commands.registerCommand('stop_listen', () => {
-            this.sttbar.off();
-            this.killed();
-        });
-        context.subscriptions.push(disposable1);
-        context.subscriptions.push(disposable2);
-        this.sttbar.setSttCmd('toggle');
-    }
-    run() {
-        if (this.sysType == 'win')
-            // console.log(this.child = this.execFile(__dirname + '/WordsMatching.exe'));
-            this.child = this.execFile(__dirname + '/WordsMatching.exe');
-        else
-            this.child = this.execFile('java', ['-jar', __dirname + '/WordsListener.jar'])
-                .on('error', err => { window.showInformationMessage('Something went wrong!!! Sorry 😢') })
-        this.child.stdout.on('data', data => {
-            window.setStatusBarMessage(data.toString(), 1000);
-            let centralCmd = new commandsClass();
-            // console.log(data.toString());
-            centralCmd.runCmd(data.toString().trim());
-        });
-
-        this.child.stderr.on('data', data => {
-            console.log(data.toString());
-        });
-    }
-    killed() {
-        this.child.kill();
-    }
+    this.child.stderr.on('data', data => {
+      console.log(data.toString())
+    })
+  }
+  killed() {
+    this.child.kill()
+  }
 }
 
 class SttBarItem {
+  private statusBarItem: vscode.StatusBarItem
+  private stt: string
 
-    private statusBarItem: StatusBarItem;
-    private stt: string;
+  constructor() {
+    this.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10)
+    this.on()
+    this.stt = 'on'
+  }
+  on() {
+    this.statusBarItem.text = '💬 listening'
+    this.statusBarItem.show()
+    this.stt = 'on'
+  }
+  off() {
+    this.statusBarItem.text = '✖️️ Stop'
+    this.statusBarItem.show()
+    this.stt = 'off'
+  }
 
-    constructor() {
-        this.statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left, 10);
-        this.on();
-        this.stt = 'on';
-    }
-    on() {
-        this.statusBarItem.text = '💬 listening';
-        this.statusBarItem.show();
-        this.stt = 'on';
-    }
-    off() {
-        this.statusBarItem.text = '✖️️ Stop';
-        this.statusBarItem.show();
-        this.stt = 'off';
-    }
+  //not in use, use setStatusBarMessage instead
+  // note(text: string) {
+  //     this.statusBarItem.text = text;
+  //     this.statusBarItem.show();
+  //     setTimeout(() => {
+  //         this.statusBarItem.text = '$(zap) listening';
+  //     }, 1000);
+  // }
 
-    //not in use, use setStatusBarMessage instead
-    // note(text: string) {
-    //     this.statusBarItem.text = text;
-    //     this.statusBarItem.show();
-    //     setTimeout(() => {
-    //         this.statusBarItem.text = '$(zap) listening';
-    //     }, 1000);
-    // }
-
-    getSttText() {
-        return this.stt;
-    }
-    setSttCmd(cmd) {
-        this.statusBarItem.command = cmd;
-    }
+  getSttText() {
+    return this.stt
+  }
+  setSttCmd(cmd) {
+    this.statusBarItem.command = cmd
+  }
 }
 
 class commandsClass {
-    runCmd(theCmd: string) {
-        switch (theCmd) {
-            case 'copy':
-                commands.executeCommand('editor.action.clipboardCopyAction');
-                break;
-            case 'cut':
-                commands.executeCommand('editor.action.clipboardCutAction');
-                break;
-            case 'delete':
-                commands.executeCommand('deleteLeft')
-                break;
-            case 'find':
-                commands.executeCommand('actions.find');
-                break;
-            case 'format':
-                commands.executeCommand('editor.action.formatDocument');
-                break;
-            case 'go to line':
-                commands.executeCommand('workbench.action.gotoLine');
-                break;
-            case 'paste':
-                commands.executeCommand('editor.action.clipboardPasteAction');
-                break;
-            case 'quick open':
-                commands.executeCommand('workbench.action.quickOpen');
-                break
-            case 'redo':
-                commands.executeCommand('redo');
-                break;
-            case 'search':
-                commands.executeCommand('workbench.view.search');
-                break;
-            case 'select all':
-                commands.executeCommand('editor.action.selectAll');
-                break;
-            case 'stop listen':
-                commands.executeCommand('stop_listen');
-                break;
-            case 'undo':
-                commands.executeCommand('undo');
-                break;
-        }
+  runCmd(theCmd: string) {
+    switch (theCmd) {
+      case 'copy':
+        vscode.commands.executeCommand('editor.action.clipboardCopyAction')
+        break
+      case 'cut':
+        vscode.commands.executeCommand('editor.action.clipboardCutAction')
+        break
+      case 'delete':
+        vscode.commands.executeCommand('deleteLeft')
+        break
+      case 'find':
+        vscode.commands.executeCommand('actions.find')
+        break
+      case 'format':
+        vscode.commands.executeCommand('editor.action.formatDocument')
+        break
+      case 'go to line':
+        vscode.commands.executeCommand('workbench.action.gotoLine')
+        break
+      case 'paste':
+        vscode.commands.executeCommand('editor.action.clipboardPasteAction')
+        break
+      case 'quick open':
+        vscode.commands.executeCommand('workbench.action.quickOpen')
+        break
+      case 'redo':
+        vscode.commands.executeCommand('redo')
+        break
+      case 'search':
+        vscode.commands.executeCommand('workbench.view.search')
+        break
+      case 'select all':
+        vscode.commands.executeCommand('editor.action.selectAll')
+        break
+      case 'stop listen':
+        vscode.commands.executeCommand('stop_listen')
+        break
+      case 'undo':
+        vscode.commands.executeCommand('undo')
+        break
     }
+  }
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
